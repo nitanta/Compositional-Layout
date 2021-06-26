ea0a4d550256ad07c043e4a6174ff24e08171356
Nitanta Adhikari <nitanta.adh007@gmail.com>
2021-06-26 11:38:12 +0545

    Initial Commit

---
+++ Easy layout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Easy layout
+//
+//  Created by Nitanta Adhikari on 6/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
